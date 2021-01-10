3aa05dc9dc2aa289145f5baa5428154a8697fe37
Joachim Kret <joachimkret@macbook.pro>
2021-01-10 17:17:07 +0100

    Added Examples project.

---
+++ Examples/Examples/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Examples
+//
+//  Created by Joachim Kret on 10/01/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ Examples/Examples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Examples
+//
+//  Created by Joachim Kret on 10/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
