06dd14c1c856c42b9ae25ac2162e8c7f9478cdc7
Joachim Kret <joachimkret@macbook.pro>
2021-01-10 17:05:57 +0100

    Initial project.

---
+++ Sources/Forms.h
@@ -0,0 +1,18 @@
+//
+//  Forms.h
+//  Forms
+//
+//  Created by Joachim Kret on 10/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Forms.
+FOUNDATION_EXPORT double FormsVersionNumber;
+
+//! Project version string for Forms.
+FOUNDATION_EXPORT const unsigned char FormsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Forms/PublicHeader.h>
+
+
